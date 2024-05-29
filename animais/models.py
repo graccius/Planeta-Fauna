4e57a8da7b9c26e8db53afbf8975670660647d92
graccius <caionicemento@gmail.com>
2024-05-29 03:53:41 -0300

    Modificando Modelos//unique//not_null

--- animais/models.py
+++ animais/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Reino(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
     imagem = models.ImageField(upload_to='reinos/')
 
     def __str__(self):
@@ -9,23 +14,39 @@ class Reino(models.Model):
 
 
 class Filo(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
     reino = models.ForeignKey(Reino, on_delete=models.CASCADE, related_name='filos')
 
     def __str__(self):
         return self.nome
     
+
 class Subfilo(models.Model):
-    nome = models.CharField(max_length=100)
-    filo = models.ForeignKey(Filo, on_delete=models.CASCADE)
+    nome = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    filo = models.ForeignKey(Filo, on_delete=models.CASCADE, related_name='subfilos')
 
     def __str__(self):
         return self.nome
     
 
 class Especie(models.Model):
-    nome = models.CharField(max_length=100)
-    subfilo = models.ForeignKey(Subfilo, on_delete=models.CASCADE)
+    nome = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    subfilo = models.ForeignKey(Subfilo, on_delete=models.CASCADE, related_name='especies')
 
     def __str__(self):
         return self.nome
